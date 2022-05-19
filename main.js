d48b99942302e8711c281bb17d31e93ff370c164
mohdbilal <mohdbilal3386@gmail.com>
2022-05-19 18:12:01 +0530

    updated digital watch

---
+++ main.js
@@ -0,0 +1,25 @@
+function currentTime() {
+    let date = new Date()
+    let hr = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    let session  = "AM"
+
+    if (hr == 0){
+        hr = 12;
+    }
+    if (hr > 12) {
+        hr = hr -12;
+        session = "PM"
+    }
+    hr = (hr < 10) ? "0" + hr : hr
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    let time  = hr + ":" + min  + ":" + sec + " " + session;
+    document.getElementById('clock').innerText = time
+
+    setTimeout("currentTime()", 1000)
+}
+
+currentTime()
